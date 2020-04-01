28267c11d1c24dc7edac97629120e67a964925c5
Tom Banish <tombanish@gmail.com>
2020-04-01 22:54:45 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -18,7 +18,12 @@ class Patient
   
   def appointments
     Appointment.all.select do |appointment|
-      appointment.patients
+      appointment.patient == self
+    end
+  end
+  
+  def doctors
+    
   end
     
   
