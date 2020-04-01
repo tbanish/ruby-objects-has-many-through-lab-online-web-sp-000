f58aa2084492475d2dac5f218ede6b6a34e62c9f
Tom Banish <tombanish@gmail.com>
2020-04-01 23:37:34 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -3,7 +3,7 @@ class Appointment
   
   @@all = []
   
-  def initialize(date, patient, doctor)
+  def initialize(date, doctor, patient)
     @date = date
     @patient = patient
     @doctor = doctor
